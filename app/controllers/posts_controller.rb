910e50c088023bf18dcca74d7f1892777a4408e6
Drakontis Panagiotis <drakontis.panagiotis@gmail.com>
2017-10-22 21:46:14 +0300

    Add kaminari for pagination

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,6 @@ class PostsController < ApplicationController
   def index
     @search = Forms::Search.new(params['forms_search'])
 
-    @posts = @search.search.first(100)
+    @posts = @search.search.page(params['page'])
   end
 end
